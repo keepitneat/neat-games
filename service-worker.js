b6d74893d3a6fa5988d11a4cf11730b5cb2be486
Ashle <ashliecrooks@gmail.com>
2026-06-05 00:23:09 -0500

    chore(sw): cache crowns/daily.js, bump CACHE_VERSION to v2

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
  * Bump CACHE_VERSION on any change to the import graph (new module -> add to
  * APP_SHELL AND bump the version). */
 
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = `neat-games-${CACHE_VERSION}`;
 
 const APP_SHELL = [
@@ -17,6 +17,7 @@ const APP_SHELL = [
   '/crowns/index.html',
   '/crowns/crowns.css',
   '/crowns/app.js',
+  '/crowns/daily.js',
   '/crowns/board.js',
   '/crowns/rules.js',
   '/crowns/solver.js',
